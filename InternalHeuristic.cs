f872077c7de1c1ba012c5b3a67c27a66fade0c44
Chase Violet <ccviol01@gmail.com>
2017-02-27 17:56:26 -0700

    Switching to ML algorithm

---
+++ InternalHeuristic.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Halite
+{
+    public static class InternalHeuristic
+    {
+
+        public static Heuristic GetInternalHeuristic(Heuristic expansionHeuristic, List<Site> edgeTargets)
+        {
+            Heuristic heuristic = expansionHeuristic.Clone();
+            heuristic.ZeroOutMySites();
+
+            List<Site> currentNodes = new List<Site>();
+            List<Site> nextNodes = new List<Site>();
+            currentNodes.AddRange(edgeTargets);
+
+            while (currentNodes.Any())
+            {
+                foreach (var node in currentNodes)
+                {
+                    double spreadValue = heuristic.Get(node).Value;
+                    spreadValue = Math.Pow(spreadValue, .9); // magic number, seems solid though.  Could reasonably increase it to .92 or so
+
+                    var spreadSites = node.Neighbors.Where(n => n.IsMine).Where(x => heuristic.Get(x).Value < spreadValue);
+                    foreach(var s in spreadSites)
+                    {
+                        heuristic.Update(s, spreadValue);
+                        nextNodes.Add(s);
+                    }
+                }
+                currentNodes = new List<Site>();
+                currentNodes.AddRange(nextNodes);
+                nextNodes = new List<Site>();
+            }
+
+            heuristic.WriteCSV("internal");
+            return heuristic;
+        }
+
+        //private static List<Site> GetPassiveNeutralNeighbors(Map m)
+        //{
+        //    return m.GetSites(s => s.Owner == 0
+        //                        && s.Neighbors.Any(n => n.IsMine)
+        //                        && s.Neighbors.All(n => !n.IsEnemy && (n.Strength > 0 || n.Neighbors.All(n2 => !n2.IsEnemy)))
+        //                        ).ToList();
+        //}
+    }
+}
---
+++ SlimeHeuristic.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Halite
+{
+    public static class SlimeHeuristic
+    {
+        public static double SpreadVal(double production, Site s) => (production) * s.Production * (255 - s.Strength) / 255; // This won't work
+
+
+        // this should only be called once per game, to get the initial field
+        public static Heuristic GetSlimeHeuristic(Heuristic startingHeuristic, Map map)
+        {
+            Heuristic slime = new Heuristic();
+
+            foreach(var site in map.GetAllSites())
+            {
+                slime.AddNew(site, site.Production, site.Strength);
+            }
+
+            for(int i=0; i < map.AreaPerPlayer; i++)
+            {
+                foreach(var site in map.GetAllSites())
+                {
+                    SpreadToNeighbors(site, slime, startingHeuristic);
+                }
+            }
+
+            foreach(var site in map.GetAllSites())
+            {
+                var val = slime.Get(site);
+                slime.Update(site, val.Production, val.Strength, val.Production / val.Strength); // impossible to break
+            }
+
+            return slime;
+        }
+
+        public static void SpreadToNeighbors(Site s, Heuristic h, Heuristic starting)
+        {
+            var siteVal = starting.Get(s);
+            foreach(var neighbor in s.ThreatZone)
+            {
+                h.AddValue(neighbor, SpreadVal(s.Production, neighbor), 0.0);
+            }
+        }
+    }
+}
